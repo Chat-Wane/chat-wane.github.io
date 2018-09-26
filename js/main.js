2718da9049e94be5a15adf5649e27a2e9e9d0495
chat-wane <grumpy.chat.wane@gmail.com>
2018-09-26 23:09:55 +0200

    scroll

--- js/main.js
+++ js/main.js
@@ -6,3 +6,25 @@ function moveNavi(element) {
     navi.style.left = -6 + rect.x + "px";
     navi.style.top = -34 + rect.y + "px";
 }
+
+let epsilon = 30;
+
+window.onscroll = function() {
+
+    let about = document.getElementById("About");
+    let publications = document.getElementById("Publications");
+    let projects = document.getElementById("Projects");
+    let contact = document.getElementById("Contact");
+    
+    if (about.getBoundingClientRect().y + about.getBoundingClientRect().height >= epsilon) {
+	moveNavi(document.getElementById("navAbout"));	
+    } else if (publications.getBoundingClientRect().y + publications.getBoundingClientRect().height >= epsilon) {
+	moveNavi(document.getElementById("navPublications"));	
+    } else if (projects.getBoundingClientRect().y + projects.getBoundingClientRect().height >= epsilon) {
+	moveNavi(document.getElementById("navProjects"));
+    } else {
+	moveNavi(document.getElementById("navContact"));
+    }
+    
+
+}
